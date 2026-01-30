eba070384b98031d74742d0e0083021a351c5f2c
aliykt3103-maker <aliykt3103@gmail.com>
2026-01-30 12:54:11 +0300

    Update arayuz.py.py

--- arayuz.py.py
+++ arayuz.py.py
@@ -3,9 +3,8 @@ import streamlit as st
 # --- AYARLAR ---
 NUMARA = "905461065331"
 
-# --- TAM ENVANTER (45 PARFÜM - NOTALAR VE YENİ RESİMLER) ---
+# --- TAM ENVANTER (HATASIZ VE EKSİKSİZ) ---
 envanter = [
-    # --- ERKEK ---
     {"ad": "Sauvage Elixir", "marka": "Dior", "fiyat": 75, "tip": "Erkek", "img": "https://pimages.parfumo.de/720/155355_img-9714-dior-sauvage-elixir_720.jpg", "notalar": "Tarçın, Kakule, Lavanta, Meyan Kökü"},
     {"ad": "Aventus", "marka": "Creed", "fiyat": 100, "tip": "Erkek", "img": "https://pimages.parfumo.de/720/156321_img-6298-creed-aventus_720.jpg", "notalar": "Ananas, Huş Ağacı, Misk, Meşe Yosunu"},
     {"ad": "Eros Parfum", "marka": "Versace", "fiyat": 75, "tip": "Erkek", "img": "https://pimages.parfumo.de/720/155700_img-4171-versace-eros-parfum_720.jpg", "notalar": "Nane, Elma, Tonka Fasulyesi, Amber"},
@@ -22,7 +21,6 @@ envanter = [
     {"ad": "Valentino Uomo Born In Roma", "marka": "Valentino", "fiyat": 75, "tip": "Erkek", "img": "https://pimages.parfumo.de/720/124785_img-2350-valentino-uomo-born-in-roma_720.jpg", "notalar": "Mineral Notalar, Tuz, Zencefil, Odunsu Notalar"},
     {"ad": "L'Aventure", "marka": "Al Haramain", "fiyat": 75, "tip": "Erkek", "img": "https://pimages.parfumo.de/720/71871_img-5364-al-haramain-l-aventure_720.jpg", "notalar": "Limon, Bergamot, Elemi, Misk, Amber"},
     
-    # --- KADIN ---
     {"ad": "Libre Intense", "marka": "YSL", "fiyat": 75, "tip": "Kadın", "img": "https://pimages.parfumo.de/720/141873_img-9169-ysl-libre-eau-de-parfum-intense_720.jpg", "notalar": "Lavanta, Portakal Çiçeği, Vanilya, Ambergris"},
     {"ad": "Delina Exclusif", "marka": "PdM", "fiyat": 100, "tip": "Kadın", "img": "https://pimages.parfumo.de/720/103328_img-9831-parfums-de-marly-delina-exclusif_720.jpg", "notalar": "Liçi, Armut, Gül, Tütsü, Oud"},
     {"ad": "Good Girl", "marka": "C. Herrera", "fiyat": 75, "tip": "Kadın", "img": "https://pimages.parfumo.de/720/79361_img-6617-carolina-herrera-good-girl_720.jpg", "notalar": "Badem, Kahve, Yasemin, Zambak, Kakao"},
@@ -32,20 +30,17 @@ envanter = [
     {"ad": "Alien", "marka": "Mugler", "fiyat": 75, "tip": "Kadın", "img": "https://pimages.parfumo.de/720/707_img-7019-mugler-alien-eau-de-parfum_720.jpg", "notalar": "Yasemin, Güneş Notaları, Beyaz Kehribar"},
     {"ad": "J'adore", "marka": "Dior", "fiyat": 75, "tip": "Kadın", "img": "https://pimages.parfumo.de/720/210_img-5645-dior-j-adore-eau-de-parfum_720.jpg", "notalar": "Armut, Kavun, Manolya, Yasemin, Orkide"},
     {"ad": "L'Interdit Rouge", "marka": "Givenchy", "fiyat": 75, "tip": "Kadın", "img": "https://pimages.parfumo.de/720/155694_img-5544-givenchy-l-interdit-eau-de-parfum-rouge_720.jpg", "notalar": "Kan Portakalı, Zencefil, Sümbülteber, Sandal Ağacı"},
-    {"ad": "Coco Mademoiselle", "marka": "Chanel", "fiyat": 75, "tip": "Kadın", "img": "https://pimages.parfumo.de/720/611_img-2826-chanel-coco-mademoiselle-eau-de-parfum_720.jpg", "notalar": "Portakal, Bergamot, Gül, Yasemin, Paçuli, Misk"},
-    {"ad": "Hypnotic Poison", "marka": "Dior", "fiyat": 75, "tip": "Kadın", "img": "https://pimages.parfumo.de/720/219_img-6246-dior-hypnotic-poison-eau-de-toilette_720.jpg", "notalar": "Hindistan Cevizi, Mürdüm Eriği, Kayısı, Sümbülteber, Vanilya"},
+    {"ad": "Coco Mademoiselle", "marka": "Chanel", "fiyat": 75, "tip": "Kadın", "img": "https://pimages.parfumo.de/720/611_img-2826-chanel-coco-mademoiselle-eau-de-parfum_720.jpg", "notalar": "Portakal, Gül, Yasemin, Paçuli, Misk"},
+    {"ad": "Hypnotic Poison", "marka": "Dior", "fiyat": 75, "tip": "Kadın", "img": "https://pimages.parfumo.de/720/219_img-6246-dior-hypnotic-poison-eau-de-toilette_720.jpg", "notalar": "Hindistan Cevizi, Eriği, Kayısı, Sümbülteber, Vanilya"},
     {"ad": "La Nuit Tresor", "marka": "Lancome", "fiyat": 75, "tip": "Kadın", "img": "https://pimages.parfumo.de/720/51296_img-2475-lancome-la-nuit-tresor-eau-de-parfum_720.jpg", "notalar": "Armut, Siyah Gül, Vanilya Orkidesi, Tütsü, Pralin"},
     
-    # --- UNİSEKS ---
     {"ad": "Baccarat Rouge 540", "marka": "MFK", "fiyat": 100, "tip": "Uniseks", "img": "https://pimages.parfumo.de/720/63510_img-1313-maison-francis-kurkdjian-baccarat-rouge-540_720.jpg", "notalar": "Safran, Yasemin, Kehribar Odunu, Çam Reçinesi"},
     {"ad": "Naxos", "marka": "Xerjoff", "fiyat": 100, "tip": "Uniseks", "img": "https://pimages.parfumo.de/720/52972_img-7546-xerjoff-1861-naxos_720.jpg", "notalar": "Lavanta, Bergamot, Bal, Tarçın, Tütün, Vanilya"},
     {"ad": "Hacivat", "marka": "Nishane", "fiyat": 100, "tip": "Uniseks", "img": "https://pimages.parfumo.de/720/97792_img-6330-nishane-hacivat_720.jpg", "notalar": "Ananas, Greyfurt, Meşe Yosunu, Sedir"},
     {"ad": "Angels' Share", "marka": "Kilian", "fiyat": 100, "tip": "Uniseks", "img": "https://pimages.parfumo.de/720/144675_img-2821-by-kilian-angels-share_720.jpg", "notalar": "Konyak, Tarçın, Meşe, Vanilya, Pralin"},
     {"ad": "Ganimede", "marka": "M.A. Barrois", "fiyat": 100, "tip": "Uniseks", "img": "https://pimages.parfumo.de/720/120935_img-6140-marc-antoine-barrois-ganymede_720.jpg", "notalar": "Safran, Menekşe Yaprağı, Ölümsüz Çiçek, Süet"},
-    {"ad": "Ani", "marka": "Nishane", "fiyat": 100, "tip": "Uniseks", "img": "https://pimages.parfumo.de/720/121113_img-2325-nishane-ani_720.jpg", "notalar": "Zencefil, Bergamot, Frenk Üzümü, Gül, Vanilya, Benzoin"},
+    {"ad": "Ani", "marka": "Nishane", "fiyat": 100, "tip": "Uniseks", "img": "https://pimages.parfumo.de/720/121113_img-2325-nishane-ani_720.jpg", "notalar": "Zencefil, Bergamot, Gül, Vanilya, Benzoin"},
     {"ad": "Erba Pura", "marka": "Xerjoff", "fiyat": 100, "tip": "Uniseks", "img": "https://pimages.parfumo.de/720/121285_img-5544-xerjoff-v-erba-pura_720.jpg", "notalar": "Sicilya Portakalı, Limon, Akdeniz Meyveleri, Beyaz Misk"},
-    {"ad": "Tobacco Vanille", "marka": "Tom Ford", "fiyat": 100, "tip": "Uniseks", "img": "https://pimages.parfumo.de/720/1825_img-5871-tom-ford-private-blend-tobacco-vanille-eau-de-parfum_720.jpg", "notalar": "Tütün Yaprağı, Baharatlar, Vanilya, Kakao, Kuru Meyveler"},
-    {"ad": "Ombre Nomade", "marka": "Louis Vuitton", "fiyat": 100, "tip": "Uniseks", "img": "https://pimages.parfumo.de/720/105437_img-4054-louis-vuitton-ombre-nomade_720.jpg", "notalar": "Oud, Ahududu, Tütsü, Safran, Gül, Huş Ağacı"},
-    {"ad": "Side Effect", "marka": "Initio", "fiyat": 100, "tip": "Uniseks", "img": "https://pimages.parfumo.de/720/82405_img-7235-initio-side-effect_720.jpg", "notalar": "Tarçın, Rom, Tütün, Vanilya, Sandal Ağacı"},
-    {"ad": "Lost Cherry", "marka": "Tom Ford", "fiyat": 100, "tip": "Uniseks", "img": "https://pimages.parfumo.de/720/111823_img-1845-tom-ford-private-blend-lost-cherry-eau-de-parfum_720.jpg", "notalar": "Vişne, Acı Badem, Likör, Türk Gülü, Tonka Fasulyesi"},
-    {"ad": "Oud Wood", "marka": "Tom Ford", "fiyat": 100, "tip": "Uniseks", "img": "https://pimages.parfumo.de/720/1826_img-2101-tom-ford-private-blend-oud-wood-eau-de-parfum_720.jpg", "notalar": "Ud Ağacı, Gülağacı, Kakule, Sandal
+    {"ad": "Tobacco Vanille", "marka": "Tom Ford", "fiyat": 100, "tip": "Uniseks", "img": "https://pimages.parfumo.de/720/1825_img-5871-tom-ford-private-blend-tobacco-vanille-eau-de-parfum_720.jpg", "notalar": "Tütün Yaprağı, Baharatlar, Vanilya, Kakao"},
+    {"ad": "Ombre Nomade", "marka": "Louis Vuitton", "fiyat": 100, "tip": "Uniseks", "img": "https://pimages.parfumo.de/720/105437_img-4054-louis-vuitton-ombre-nomade_720.jpg", "notalar": "Oud, Ahududu, Tütsü, Safran, Gül"},
+    {"ad": "Side Effect", "marka": "Initio", "fiyat": 100, "tip": "Uniseks", "img": "
