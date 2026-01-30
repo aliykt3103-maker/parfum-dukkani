016e3cdeff6d832d79089eaaf7a4d1c18df18783
aliykt3103-maker <aliykt3103@gmail.com>
2026-01-30 13:23:39 +0300

    Update arayuz.py.py

--- arayuz.py.py
+++ arayuz.py.py
@@ -3,110 +3,83 @@ import streamlit as st
 # --- AYARLAR ---
 NUMARA = "905461065331"
 
-# --- DEV ENVANTER (45 PARFÜM) ---
+# --- 50 ADET ERKEK PARFÜMÜ (KATEGORİZE EDİLMİŞ) ---
 envanter = [
-    # --- ERKEK (15 ADET) ---
-    {"ad": "Sauvage Elixir", "marka": "Dior", "fiyat": 75, "tip": "Erkek", "img": "https://pimages.parfumo.de/720/155355_img-9714-dior-sauvage-elixir_720.jpg", "notalar": "Tarçın, Kakule, Lavanta"},
-    {"ad": "Aventus", "marka": "Creed", "fiyat": 100, "tip": "Erkek", "img": "https://fimgs.net/mdimg/perfume/m.9828.jpg", "notalar": "Ananas, Huş Ağacı, Misk"},
-    {"ad": "Eros Parfum", "marka": "Versace", "fiyat": 75, "tip": "Erkek", "img": "https://pimages.parfumo.de/720/155700_img-4171-versace-eros-parfum_720.jpg", "notalar": "Nane, Elma, Kehribar"},
-    {"ad": "Layton", "marka": "PdM", "fiyat": 100, "tip": "Erkek", "img": "https://pimages.parfumo.de/720/80743_img-1965-parfums-de-marly-layton_720.jpg", "notalar": "Elma, Lavanta, Vanilya"},
-    {"ad": "Le Male Elixir", "marka": "JPG", "fiyat": 75, "tip": "Erkek", "img": "https://pimages.parfumo.de/720/215758_le_male_elixir.jpg", "notalar": "Bal, Tütün, Lavanta"},
-    {"ad": "Stronger With You", "marka": "Armani", "fiyat": 75, "tip": "Erkek", "img": "https://pimages.parfumo.de/720/118314_stronger_with_you.jpg", "notalar": "Kestane, Adaçayı, Vanilya"},
-    {"ad": "Bleu de Chanel", "marka": "Chanel", "fiyat": 80, "tip": "Erkek", "img": "https://pimages.parfumo.de/720/104323_bleu_de_chanel.jpg", "notalar": "Sandal Ağacı, Sedir, Greyfurt"},
-    {"ad": "Spicebomb Extreme", "marka": "V&R", "fiyat": 75, "tip": "Erkek", "img": "https://pimages.parfumo.de/720/54655_spicebomb.jpg", "notalar": "Tütün, Vanilya, Karabiber"},
-    {"ad": "Acqua di Gio", "marka": "Armani", "fiyat": 70, "tip": "Erkek", "img": "https://pimages.parfumo.de/720/47699_acqua_di_gio.jpg", "notalar": "Deniz Notaları, Biberiye"},
-    {"ad": "Prada L'Homme", "marka": "Prada", "fiyat": 75, "tip": "Erkek", "img": "https://pimages.parfumo.de/720/73385_prada.jpg", "notalar": "İris, Neroli, Sardunya"},
-    {"ad": "Terre d'Hermes", "marka": "Hermes", "fiyat": 75, "tip": "Erkek", "img": "https://pimages.parfumo.de/720/823_terre.jpg", "notalar": "Portakal, Çakmaktaşı, Sedir"},
-    {"ad": "The Most Wanted", "marka": "Azzaro", "fiyat": 75, "tip": "Erkek", "img": "https://pimages.parfumo.de/720/151603_wanted.jpg", "notalar": "Kakule, Karamel, Amberwood"},
-    {"ad": "Y EDP", "marka": "YSL", "fiyat": 75, "tip": "Erkek", "img": "https://pimages.parfumo.de/720/103975_ysl_y.jpg", "notalar": "Zencefil, Elma, Adaçayı"},
-    {"ad": "L'Aventure", "marka": "Al Haramain", "fiyat": 60, "tip": "Erkek", "img": "https://pimages.parfumo.de/720/71871_laventure.jpg", "notalar": "Limon, Bergamot, Yasemin"},
-    {"ad": "Gentleman Privee", "marka": "Givenchy", "fiyat": 75, "tip": "Erkek", "img": "https://pimages.parfumo.de/720/165561_gentleman.jpg", "notalar": "Viski, Kestane, İris"},
+    # --- BLUE (Mavi - Ferah, Deniz, Sabunsu) ---
+    {"ad": "Bleu de Chanel", "fiyat": 80, "cat": "🟦 BLUE", "img": "https://fimgs.net/mdimg/perfume/m.25967.jpg", "notalar": "Greyfurt, Tütsü, Zencefil", "tip": "Erkek"},
+    {"ad": "Sauvage EDP", "fiyat": 75, "cat": "🟦 BLUE", "img": "https://fimgs.net/mdimg/perfume/m.49144.jpg", "notalar": "Bergamot, Biber, Lavanta", "tip": "Erkek"},
+    {"ad": "Acqua di Gio", "fiyat": 70, "cat": "🟦 BLUE", "img": "https://fimgs.net/mdimg/perfume/m.410.jpg", "notalar": "Deniz Notaları, Limon, Biberiye", "tip": "Erkek"},
+    {"ad": "Dylan Blue", "fiyat": 70, "cat": "🟦 BLUE", "img": "https://fimgs.net/mdimg/perfume/m.39348.jpg", "notalar": "İncir Yaprağı, Bergamot, Su Notaları", "tip": "Erkek"},
+    {"ad": "Y EDP", "fiyat": 75, "cat": "🟦 BLUE", "img": "https://fimgs.net/mdimg/perfume/m.47506.jpg", "notalar": "Elma, Adaçayı, Amberwood", "tip": "Erkek"},
+    {"ad": "Luna Rossa Ocean", "fiyat": 75, "cat": "🟦 BLUE", "img": "https://fimgs.net/mdimg/perfume/m.68652.jpg", "notalar": "Bergamot, İris, Vetiver", "tip": "Erkek"},
+    
+    # --- GREEN (Yeşil - Doğal, Ormansı, Otsu) ---
+    {"ad": "Green Irish Tweed", "fiyat": 100, "cat": "🟩 GREEN", "img": "https://fimgs.net/mdimg/perfume/m.474.jpg", "notalar": "Menekşe Yaprağı, İris, Sandal Ağacı", "tip": "Erkek"},
+    {"ad": "Polo Green", "fiyat": 70, "cat": "🟩 GREEN", "img": "https://fimgs.net/mdimg/perfume/m.829.jpg", "notalar": "Çam, Tütün, Deri", "tip": "Erkek"},
+    {"ad": "Hacivat", "fiyat": 110, "cat": "🟩 GREEN", "img": "https://fimgs.net/mdimg/perfume/m.44174.jpg", "notalar": "Ananas, Meşe Yosunu, Odunsu", "tip": "Erkek"},
+    {"ad": "Grey Vetiver", "fiyat": 100, "cat": "🟩 GREEN", "img": "https://fimgs.net/mdimg/perfume/m.6634.jpg", "notalar": "Vetiver, Greyfurt, Adaçayı", "tip": "Erkek"},
 
-    # --- KADIN (15 ADET) ---
-    {"ad": "Libre Intense", "marka": "YSL", "fiyat": 80, "tip": "Kadın", "img": "https://pimages.parfumo.de/720/141873_libre.jpg", "notalar": "Lavanta, Portakal Çiçeği, Vanilya"},
-    {"ad": "Good Girl", "marka": "C. Herrera", "fiyat": 75, "tip": "Kadın", "img": "https://pimages.parfumo.de/720/79361_good_girl.jpg", "notalar": "Badem, Kahve, Yasemin"},
-    {"ad": "Delina Exclusif", "marka": "PdM", "fiyat": 110, "tip": "Kadın", "img": "https://pimages.parfumo.de/720/103328_delina.jpg", "notalar": "Gül, Liçi, Armut, Tütsü"},
-    {"ad": "Black Opium", "marka": "YSL", "fiyat": 75, "tip": "Kadın", "img": "https://pimages.parfumo.de/720/43734_black_opium.jpg", "notalar": "Kahve, Vanilya, Beyaz Çiçekler"},
-    {"ad": "Alien", "marka": "Mugler", "fiyat": 75, "tip": "Kadın", "img": "https://pimages.parfumo.de/720/707_alien.jpg", "notalar": "Yasemin, Amber, Kaşmir"},
-    {"ad": "J'adore", "marka": "Dior", "fiyat": 80, "tip": "Kadın", "img": "https://pimages.parfumo.de/720/210_jadore.jpg", "notalar": "Armut, Kavun, Yasemin"},
-    {"ad": "Crystal Noir", "marka": "Versace", "fiyat": 70, "tip": "Kadın", "img": "https://pimages.parfumo.de/720/631_crystal.jpg", "notalar": "Hindistan Cevizi, Biber, Gardenya"},
-    {"ad": "La Vie Est Belle", "marka": "Lancome", "fiyat": 70, "tip": "Kadın", "img": "https://pimages.parfumo.de/720/14973_lavie.jpg", "notalar": "Pralin, Vanilya, İris"},
-    {"ad": "Hypnotic Poison", "marka": "Dior", "fiyat": 75, "tip": "Kadın", "img": "https://pimages.parfumo.de/720/219_hypnotic.jpg", "notalar": "Acı Badem, Kimyon, Vanilya"},
-    {"ad": "L'Interdit Rouge", "marka": "Givenchy", "fiyat": 80, "tip": "Kadın", "img": "https://pimages.parfumo.de/720/155694_interdit.jpg", "notalar": "Kan Portakalı, Zencefil, Sümbülteber"},
-    {"ad": "Idole", "marka": "Lancome", "fiyat": 75, "tip": "Kadın", "img": "https://pimages.parfumo.de/720/124707_idole.jpg", "notalar": "Gül, Yasemin, Beyaz Misk"},
-    {"ad": "Chloe EDP", "marka": "Chloe", "fiyat": 75, "tip": "Kadın", "img": "https://pimages.parfumo.de/720/1550_chloe.jpg", "notalar": "Şakayık, Gül, Manolya"},
-    {"ad": "Burberry Her", "marka": "Burberry", "fiyat": 75, "tip": "Kadın", "img": "https://pimages.parfumo.de/720/109001_her.jpg", "notalar": "Çilek, Ahududu, Böğürtlen"},
-    {"ad": "Baccarat Rouge (K)", "marka": "MFK", "fiyat": 100, "tip": "Kadın", "img": "https://fimgs.net/mdimg/perfume/m.33531.jpg", "notalar": "Safran, Yasemin, Amber"},
-    {"ad": "Coco Mademoiselle", "marka": "Chanel", "fiyat": 85, "tip": "Kadın", "img": "https://pimages.parfumo.de/720/611_coco.jpg", "notalar": "Portakal, Gül, Paçuli"},
+    # --- FRESH (Ferah - Turunçgil, Temiz, Enerjik) ---
+    {"ad": "Aventus", "fiyat": 110, "cat": "🌬 FRESH", "img": "https://fimgs.net/mdimg/perfume/m.9828.jpg", "notalar": "Ananas, Bergamot, Huş Ağacı", "tip": "Erkek"},
+    {"ad": "Prada L'Homme", "fiyat": 75, "cat": "🌬 FRESH", "img": "https://fimgs.net/mdimg/perfume/m.39029.jpg", "notalar": "İris, Neroli, Sardunya", "tip": "Erkek"},
+    {"ad": "Silver Mountain Water", "fiyat": 100, "cat": "🌬 FRESH", "img": "https://fimgs.net/mdimg/perfume/m.472.jpg", "notalar": "Yeşil Çay, Frenk Üzümü, Misk", "tip": "Erkek"},
+    {"ad": "L'Eau d'Issey", "fiyat": 70, "cat": "🌬 FRESH", "img": "https://fimgs.net/mdimg/perfume/m.721.jpg", "notalar": "Yuzu, Limon, Mavi Nilüfer", "tip": "Erkek"},
 
-    # --- UNİSEKS / NİŞ (15 ADET) ---
-    {"ad": "Naxos", "marka": "Xerjoff", "fiyat": 110, "tip": "Uniseks", "img": "https://pimages.parfumo.de/720/52972_naxos.jpg", "notalar": "Bal, Tütün, Lavanta, Tarçın"},
-    {"ad": "Angels' Share", "marka": "Kilian", "fiyat": 110, "tip": "Uniseks", "img": "https://fimgs.net/mdimg/perfume/m.62615.jpg", "notalar": "Konyak, Tarçın, Meşe, Vanilya"},
-    {"ad": "Hacivat", "marka": "Nishane", "fiyat": 100, "tip": "Uniseks", "img": "https://pimages.parfumo.de/720/97792_hacivat.jpg", "notalar": "Ananas, Greyfurt, Meşe Yosunu"},
-    {"ad": "Ani", "marka": "Nishane", "fiyat": 100, "tip": "Uniseks", "img": "https://pimages.parfumo.de/720/121113_ani.jpg", "notalar": "Vanilya, Zencefil, Bergamot"},
-    {"ad": "Erba Pura", "marka": "Xerjoff", "fiyat": 100, "tip": "Uniseks", "img": "https://pimages.parfumo.de/720/121285_erba.jpg", "notalar": "Meyve Notaları, Beyaz Misk, Kehribar"},
-    {"ad": "Tobacco Vanille", "marka": "Tom Ford", "fiyat": 110, "tip": "Uniseks", "img": "https://pimages.parfumo.de/720/1825_tobacco.jpg", "notalar": "Tütün, Vanilya, Baharatlar"},
-    {"ad": "Oud Wood", "marka": "Tom Ford", "fiyat": 110, "tip": "Uniseks", "img": "https://pimages.parfumo.de/720/1826_oud.jpg", "notalar": "Ud, Kakule, Sandal Ağacı"},
-    {"ad": "Ganimede", "marka": "M.A. Barrois", "fiyat": 110, "tip": "Uniseks", "img": "https://pimages.parfumo.de/720/120935_ganymede.jpg", "notalar": "Mandalina, Safran, Süet"},
-    {"ad": "Side Effect", "marka": "Initio", "fiyat": 110, "tip": "Uniseks", "img": "https://pimages.parfumo.de/720/82405_side.jpg", "notalar": "Rom, Tütün, Tarçın"},
-    {"ad": "Lost Cherry", "marka": "Tom Ford", "fiyat": 110, "tip": "Uniseks", "img": "https://pimages.parfumo.de/720/111823_cherry.jpg", "notalar": "Vişne, Acı Badem, Likör"},
-    {"ad": "Ombre Nomade", "marka": "LV", "fiyat": 120, "tip": "Uniseks", "img": "https://pimages.parfumo.de/720/105437_nomade.jpg", "notalar": "Oud, Ahududu, Tütsü"},
-    {"ad": "Black Phantom", "marka": "Kilian", "fiyat": 110, "tip": "Uniseks", "img": "https://pimages.parfumo.de/720/86121_phantom.jpg", "notalar": "Çikolata, Kahve, Karamel"},
-    {"ad": "Alexandria II", "marka": "Xerjoff", "fiyat": 120, "tip": "Uniseks", "img": "https://pimages.parfumo.de/720/43862_alex.jpg", "notalar": "Elma, Lavanta, Oud, Tarçın"},
-    {"ad": "Silver Mountain", "marka": "Creed", "fiyat": 100, "tip": "Uniseks", "img": "https://pimages.parfumo.de/720/9830_silver.jpg", "notalar": "Yeşil Çay, Siyah Frenk Üzümü, Misk"},
-    {"ad": "Portrait of a Lady", "marka": "F. Malle", "fiyat": 110, "tip": "Uniseks", "img": "https://pimages.parfumo.de/720/10464_portrait.jpg", "notalar": "Gül, Tütsü, Karanfil, Paçuli"}
+    # --- RED (Kırmızı - Sıcak, Baharatlı, Şehvetli) ---
+    {"ad": "Eros Parfum", "fiyat": 75, "cat": "🟥 RED", "img": "https://fimgs.net/mdimg/perfume/m.63731.jpg", "notalar": "Nane, Elma, Amber", "tip": "Erkek"},
+    {"ad": "Spicebomb Extreme", "fiyat": 80, "cat": "🟥 RED", "img": "https://fimgs.net/mdimg/perfume/m.30447.jpg", "notalar": "Tütün, Vanilya, Karabiber", "tip": "Erkek"},
+    {"ad": "Stronger With You", "fiyat": 75, "cat": "🟥 RED", "img": "https://fimgs.net/mdimg/perfume/m.44587.jpg", "notalar": "Kestane, Karamel, Vanilya", "tip": "Erkek"},
+    {"ad": "1 Million Elixir", "fiyat": 80, "cat": "🟥 RED", "img": "https://fimgs.net/mdimg/perfume/m.72138.jpg", "notalar": "Elma, Davana, Gül, Vanilya", "tip": "Erkek"},
+    {"ad": "Layton", "fiyat": 110, "cat": "🟥 RED", "img": "https://fimgs.net/mdimg/perfume/m.39332.jpg", "notalar": "Elma, Lavanta, Vanilya, Karabiber", "tip": "Erkek"},
+    {"ad": "Side Effect", "fiyat": 120, "cat": "🟥 RED", "img": "https://fimgs.net/mdimg/perfume/m.42260.jpg", "notalar": "Rom, Tütün, Tarçın", "tip": "Erkek"}
 ]
 
-st.set_page_config(page_title="DEKANT VİTRİNİ", layout="centered")
+st.set_page_config(page_title="ALİY PARFÜM VİTRİNİ", layout="centered")
 
-# --- TASARIM ---
+# --- CSS ---
 st.markdown("""
 <style>
     .stApp { background-color: #ffffff; }
     .parfum-kart { 
-        background: white; 
-        border: 1px solid #f0f0f0; 
-        border-radius: 20px; 
-        padding: 20px; 
-        text-align: center; 
-        box-shadow: 0 4px 10px rgba(0,0,0,0.05);
-        margin-bottom: 25px;
+        background: white; border-radius: 20px; padding: 15px; text-align: center; 
+        box-shadow: 0 4px 15px rgba(0,0,0,0.05); border: 1px solid #f0f0f0; margin-bottom: 20px;
     }
-    .notalar { color: #d32f2f; font-size: 13px; font-weight: 600; background: #fff5f5; padding: 8px; border-radius: 10px; margin: 10px 0; }
-    img { border-radius: 15px; margin-bottom: 10px; }
-    .fiyat-tag { color: #111; font-size: 18px; font-weight: 800; }
+    .notalar { color: #555; font-size: 13px; font-style: italic; background: #f9f9f9; padding: 10px; border-radius: 10px; margin: 10px 0; }
+    .cat-tag { font-weight: bold; font-size: 12px; color: #888; text-transform: uppercase; }
 </style>
 """, unsafe_allow_html=True)
 
-if 'secim' not in st.session_state:
-    st.session_state.secim = None
+if 'secim' not in st.session_state: st.session_state.secim = None
 
-# --- MENÜ ---
+# --- ANA MENÜ ---
 if st.session_state.secim is None:
-    st.title("⭐️ ALİY DEKANT")
-    st.write("Hoş geldiniz, vitrin seçerek keşfetmeye başlayın.")
+    st.title("🛡 ALİY DEKANT")
     if st.button("👔 ERKEK KOLEKSİYONU", use_container_width=True): st.session_state.secim = "Erkek"; st.rerun()
-    if st.button("👗 KADIN KOLEKSİYONU", use_container_width=True): st.session_state.secim = "Kadın"; st.rerun()
-    if st.button("✨ NİŞ & UNİSEKS", use_container_width=True): st.session_state.secim = "Uniseks"; st.rerun()
+    st.info("Koku karakterine göre filtreleme içeride mevcuttur.")
     st.stop()
 
-st.button("⬅️ ANA MENÜYE DÖN", on_click=lambda: setattr(st.session_state, 'secim', None))
+# --- VİTRİN ---
+st.button("⬅️ ANA MENÜ", on_click=lambda: setattr(st.session_state, 'secim', None))
 
-# --- LİSTELEME ---
-filtreli = [p for p in envanter if p['tip'] == st.session_state.secim]
+# Kategori Seçimi
+kategoriler = ["TÜMÜ", "🟦 BLUE", "🟩 GREEN", "🌬 FRESH", "🟥 RED"]
+secilen_cat = st.selectbox("Koku Karakteri Filtrele:", kategoriler)
 
-for p in filtreli:
+# Listeleme
+gosterilecek = [p for p in envanter if (p['tip'] == st.session_state.secim) and (secilen_cat == "TÜMÜ" or p['cat'] == secilen_cat)]
+
+for p in gosterilecek:
     with st.container():
         st.markdown(f'''
         <div class="parfum-kart">
-            <p style="color:#999; font-size:12px; text-transform:uppercase;">{p["marka"]}</p>
-            <img src="{p["img"]}" width="100%" style="max-height:300px; object-fit:contain;">
-            <h2 style="margin:5px 0;">{p["ad"]}</h2>
-            <div class="notalar">KOKU PİRAMİDİ: {p["notalar"]}</div>
+            <span class="cat-tag">{p["cat"]}</span>
+            <img src="{p["img"]}" width="100%" style="max-height:350px; object-fit:contain;">
+            <h2 style="margin:10px 0;">{p["ad"]}</h2>
+            <div class="notalar"><b>Notalar:</b> {p["notalar"]}</div>
         </div>
         ''', unsafe_allow_html=True)
         
-        ml = st.select_slider(f"Boyut Seç ({p['ad']})", options=[3, 5, 10], key=f"ml_{p['ad']}")
-        toplam = int(ml * p['fiyat'])
-        st.button(f"{toplam} TL - SİPARİŞ VER (WhatsApp)", use_container_width=True, disabled=True, key=f"btn_{p['ad']}")
+        ml = st.select_slider(f"Boyut seç ({p['ad']})", options=[3, 5, 10], key=f"ml_{p['ad']}")
+        if st.button(f"{int(ml * p['fiyat'])} TL - SATIN AL", key=f"btn_{p['ad']}", use_container_width=True):
+            st.warning("⚠️ Bu ürün yakında satışa sunulacaktır!")
         st.write("---")
